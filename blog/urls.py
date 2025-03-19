b2059385828aad01b3011615d06516bba8092668
oguntayo <oguntayohabeebullah@gmail.com>
2025-03-19 17:29:06 +0100

    fix:test CD

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ from .views import (
 urlpatterns = [
     path("", BlogListCreateView.as_view(), name="blog-list-create"),
     path("<int:pk>/", BlogDetailView.as_view(), name="blog-detail"),
-    path("<int:blog_id>/comments/", CommentListCreateView.as_view(), name="blog-comments"),
+    path("<int:blog_id>/commentsss/", CommentListCreateView.as_view(), name="blog-comments"),
     path("<int:blog_id>/like/", LikeBlogView.as_view(), name="blog-like"),
     path("comments/<int:comment_id>/like/", LikeCommentView.as_view(), name="comment-like"),
 ]
