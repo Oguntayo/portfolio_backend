33e61b9cf04ce622901bf70d41fe2732b68ade3d
oguntayo <oguntayohabeebullah@gmail.com>
2025-03-25 12:52:15 +0100

    fix:django auth error TypeError: 'type' object is not iterable

--- backend/settings.py
+++ backend/settings.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
 import warnings
-from rest_framework_simplejwt.tokens import AccessToken
 
 warnings.filterwarnings("ignore", category=UserWarning, module="allauth.account.app_settings")
 
@@ -152,14 +151,12 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 }
 
-
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=int(os.getenv("ACCESS_TOKEN_LIFETIME", 1))),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=int(os.getenv("REFRESH_TOKEN_LIFETIME", 7))),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
-    'AUTH_HEADER_TYPES': ('Bearer',),
-    "AUTH_TOKEN_CLASSES": [AccessToken],  
+    'AUTH_HEADER_TYPES': ('Bearer',)
 }
 
 REST_USE_JWT = True
