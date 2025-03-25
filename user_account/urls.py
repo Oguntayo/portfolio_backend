67d8d7c8a1e0dea779f340a84aa8415b39d57a0d
oguntayo <oguntayohabeebullah@gmail.com>
2025-03-25 03:45:52 +0100

    fix:update SOCIALACCOUNT_AUTO_SIGNUP to be false

--- user_account/urls.py
+++ user_account/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 from .views import RegisterView, LoginView,LogoutView,ProfileView
+from django.urls import path
+from django.http import HttpResponse
+
+def dummy_signup_view(request):
+    return HttpResponse("Signup not allowed.", status=403)
+
+urlpatterns = [
+    path("accounts/social/signup/", dummy_signup_view, name="socialaccount_signup"),  # Prevents NoReverseMatch
+]
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'), 
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'), 
     path("profile/", ProfileView.as_view(), name="account-profile"),
+    path("accounts/social/signup/", dummy_signup_view, name="socialaccount_signup"),
 ]
