52f0b80efa758f2f3af3f8c5852580c1d3f93615
oguntayo <oguntayohabeebullah@gmail.com>
2025-03-25 21:24:26 +0100

    fix:update requirements.txt

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/oh/portfolio_backend/venv"
+set -gx VIRTUAL_ENV "/home/oh/Portfolio/portfolio_backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
