e1b5f3932232574031d4b218497ca4b5bd156548
oguntayo <oguntayohabeebullah@gmail.com>
2025-03-25 02:05:22 +0100

    fix:add FETCH_USER_INFO: False to social account provider

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-default-key")
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost,http://localhost:5173").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 # Database Configuration (railway PostgreSQL)
 DATABASES = {
@@ -173,6 +173,7 @@ SOCIALACCOUNT_PROVIDERS = {
         "SCOPE": ["email", "profile"],
         "AUTH_PARAMS": {"access_type": "offline"},
         "OAUTH_PKCE_ENABLED": True,
+        "FETCH_USER_INFO": False,
     }
 }
 
@@ -190,3 +191,4 @@ SWAGGER_SETTINGS = {
 }
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
