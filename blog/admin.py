adddc5e6902c84b33f417c0cc93175aef5dd5dd7
oguntayo <oguntayohabeebullah@gmail.com>
2025-03-26 22:54:27 +0100

    fix:register model to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Blog, Comment
 
-# Register your models here.
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "created_at", "total_likes")
+    search_fields = ("title", "author__email")
+    list_filter = ("created_at",)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("blog", "author", "created_at", "total_likes")
+    search_fields = ("blog__title", "author__email", "content")
+    list_filter = ("created_at",)
+
--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Project, ProjectImage, Review
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "completion_date", "featured", "average_rating", "review_count")
+    search_fields = ("title", "description", "technologies", "tags")
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("featured", "completion_date")
+    ordering = ("-created_at",)
+
+@admin.register(ProjectImage)
+class ProjectImageAdmin(admin.ModelAdmin):
+    list_display = ("project", "uploaded_at")
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("project", "reviewer", "rating", "created_at")
+    list_filter = ("rating", "created_at")
+    search_fields = ("project__title", "reviewer__username", "comment")
+
